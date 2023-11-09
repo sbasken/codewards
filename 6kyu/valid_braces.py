82f25c40afe3003c68d2f8bdc3ee5dcc1db91072
sbasken <120995506+sbasken@users.noreply.github.com>
2023-11-09 12:09:05 -0800

    Create valid_braces.py

---
+++ 6kyu/valid_braces.py
@@ -0,0 +1,33 @@
+# Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+
+# This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+
+# All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+# What is considered Valid?
+# A string of braces is considered valid if all braces are matched with the correct brace.
+
+# Examples
+# "(){}[]"   =>  True
+# "([{}])"   =>  True
+# "(}"       =>  False
+# "[(])"     =>  False
+# "[({})](]" =>  False
+
+def valid_braces(string):
+    if len(string) % 2 == 1: 
+        return False
+    leftover = string
+    curly = "{}"
+    square = "[]"
+    normal = "()"
+    halflen = int(len(string)/2)
+    for i in range(halflen):
+        print(leftover)
+        if curly in leftover:
+            leftover = leftover.replace(curly, '')
+        elif square in leftover:
+            leftover = leftover.replace(square, '')
+        elif normal in leftover:
+            print("hello!")
+            leftover = leftover.replace("()","")
